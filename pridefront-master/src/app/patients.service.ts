56c284d9bd642559f079c974e8e3761c2dbf384d
jessicatrettin <jessica.trettin@gmail.com>
2021-10-08 21:31:49 -0300

    com patients.service

--- pridefront-master/src/app/patients.service.ts
+++ pridefront-master/src/app/patients.service.ts
@@ -1,9 +1,27 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
 @Injectable({
   providedIn: 'root'
 })
-export class PatientsService {
+export class PatientService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  private BASE_URL = "https://localhost:44328/api/Patients" 
+
+  getPatients(){
+    this.http.get(this.BASE_URL).subscribe(data => {
+      debugger;
+      console.log(data);
+    });
+  }
+
+  getPatient(email:string)
+  {
+    this.http.get(this.BASE_URL + "/Medico/Ginecologista").subscribe(data => {
+      debugger;
+      console.log(data);
+    });
+  }
 }
